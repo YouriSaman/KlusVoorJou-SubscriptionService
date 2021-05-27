e6fa0bc1791d191dcf5d4162633925c240f25ab9
Youri Saman <youri.saman10@gmail.com>
2021-05-27 19:51:13 +0200

    Init commit

---
+++ SubscriptionService/Controllers/SubscriptionController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SubscriptionService.Controllers
+{
+    public class SubscriptionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
